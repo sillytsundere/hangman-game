497d4cfca2f70b1cdb7153aeebd7d2e7e172721d
Paige <paigehcarroll@gmail.com>
2023-07-18 19:02:40 -0500

    debugged timer, get it its own function, created array and function to create letter buttons when game starts

--- assets/script.js
+++ assets/script.js
@@ -1,33 +1,80 @@
 var startbutton = document.querySelector('#start');
 var countdownEl = document.getElementById("timer");
+var letterSection = document.querySelector('.letter-buttons');
 
 var startingMinutes = 1;
-var time = startingMinutes * 60;
+var time = startingMinutes * 10;
+
+var letters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 
 var lose = function(){
-    document.getElementById('main').display = none;
-    document.querySelector('#lose').display = block;
-}
-var win = function(){
-    document.querySelector('.win').display = block;
+    document.getElementById('main').style.display = 'none';
+    document.querySelector('#lose').style.display = 'block';
 }
 
-startbutton.addEventListener('click', function() {
+var win = function(){
+    document.querySelector('.win').style.display = 'block';
+}
+
+function getRandomIndex (arr) {
+	var randomIndex = Math.floor(Math.random() * arr.length);
+    return randomIndex;
+}
+
+var wordBank = [
+    {
+        word: 'parsnip',
+        hint: 'Dissappointing root vegatable.'
+    },
+    {
+        word: 'apple',
+        hint: 'Nothing worse than finding a worm in this'
+    },
+    {
+        word: 'crayon',
+        hint: 'Not a crown'
+    }
+]
+
+let wordIndex = getRandomIndex (wordBank);
+
+function writeButtons() {
+    console.log('writing the game buttons');
+    for (var i = 0; i < letters.length; i++) {
+        var buttons = document.createElement('button');
+        buttons.textContent = letters[i];
+        letterSection.appendChild(buttons);
+    }
+}
+
+function timeLimit() {
     console.log('event listener works');
-    setInterval(function() {
-        var minutes = Math.floor(time / 60);
-        var seconds = time % 60;
     
+    let minutes = Math.floor(time / 60);
+    let seconds = time % 60;
+
+    countdownEl.innerHTML = minutes + ':' + seconds;
+
+    var timer = setInterval(function() {
+        time--;
+
+        let minutes = Math.floor(time / 60);
+        let seconds = time % 60;
+
         if (seconds < 10) {
             seconds = '0' + seconds;
         } 
-    
+
         countdownEl.innerHTML = minutes + ':' + seconds;
-        time--;
     
-        if (time === 0) {
-            clearInterval();
+        if (time <= 0) {
+            clearInterval(timer);
             lose();
         }
     }, 1000);
+}
+
+startbutton.addEventListener('click', function() {
+    timeLimit();
+    writeButtons();
 });
