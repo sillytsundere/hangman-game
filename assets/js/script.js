4cde696c2a4dda9e850c7e34e8e05e9fd1e68034
Paige <paigehcarroll@gmail.com>
2023-08-09 18:31:43 -0500

    added display if statement for word to be guessed

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,15 @@
-var startButton = document.querySelector('#start');
-var countdownEl = document.getElementById("timer");
-var turnsHolder = document.getElementById('my-turns');
-var wordHolder = document.getElementById('word-place');
+const startButton = document.querySelector('#start');
+const countdownEl = document.getElementById("timer");
+const turnsHolder = document.getElementById('my-turns');
+const wordHolder = document.getElementById('word-place');
 
 var startingMinutes = 1;
 var time = startingMinutes * 60;
 let wordIndex;
 let turns;
 let roundWord;
-let wordStatus = null;
+//let wordStatus = null;
+let showWord;
 
 var letters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 
@@ -65,16 +66,19 @@ var winDisplay = function(){
     document.getElementById('win').style.display = 'block';
 }
 
+//chooses random index for length of word bank array to choose a random word to guess, called upon game start to choose a new word for each new game
 function getRandomIndex (arr) {
 	var randomIndex = Math.floor(Math.random() * arr.length);
     return randomIndex;
 }
 
+//function called in event listener when Begin button is clicked-begins game
 function startGame() {
     wordIndex = getRandomIndex(wordBank);
     turns = 7;
     turnsHolder.innerHTML = `0${turns}`;
     roundWord = wordBank[wordIndex].word.split('');
+    showWord = wordBank[wordIndex].word.split('');
     console.log(roundWord, 'round word');
     hideBtn();
     timeLimit();
@@ -84,14 +88,23 @@ function startGame() {
 
 }
 
+//displays chosen word on webpage as string of dashes to symbolize how many letters user has to guess and to serve to display correct letters user has guessed
 function writeWord() {
-    wordStatus = roundWord.map(guessedChar => (roundWord.indexOf(guessedChar) >= 0 ? " _ " : guessedChar)).join('');
-    wordHolder.innerHTML = wordStatus;
+    // wordStatus = roundWord.map((char) => char.replace(char, " _ "));
+    // console.log(wordStatus, 'word status-the dashed array of the round word');
+    // wordHolder.innerHTML = wordStatus.join('');
+    for (var i = 0; i < roundWord.length; i++) {
+        var word = document.createElement('span');
+        word.classList.add('underline');
+        word.classList.add('hidden');
+        word.textContent = roundWord[i];
+        wordHolder.appendChild(word);
+    }
 }
 
 //this function is selecting all the letter buttons and listening for a click on them
 function clickLetters() {
-    var allBtns = document.querySelectorAll('.char');
+    const allBtns = document.querySelectorAll('.char');
     allBtns.forEach(function(btn) {
         btn.addEventListener('click', function(){
             console.log(checkAnswer(this.innerHTML));
@@ -103,18 +116,44 @@ function clickLetters() {
     })
 }
 
+//need to make function to push letters that are guessed correctly to the word status array 
+//will want to use .replace to add the correct letters to the dashed word array
+// function updateWord() {
+//     console.log(wordStatus, 'wordStatus in updateWord ftn');
+//     console.log('update word');
+//     //need to have index of correct letter in word array and then push or replace it to its correct place to display on webpage
+// }
+
+
 function checkAnswer(inputChar) {
     for (var i = 0; i < roundWord.length; i++) {
         if (inputChar === roundWord[i]) {
+            console.log(roundWord[i], 'before index is removed');
 
             //remove one char of that from roundWord
             var indexToRemove = roundWord.indexOf(inputChar);
+            console.log(indexToRemove, 'index of char to be removed');
             var temp = roundWord.slice(0, indexToRemove);
-            
             var temp1 = roundWord.slice(indexToRemove +1);
             
             roundWord = temp.concat(temp1);
-            console.log(roundWord);
+            console.log(roundWord, 'after index is removed');
+
+            // console.log(wordStatus, 'word status in check answer ftn');
+
+            var chars = wordHolder.children;
+            for (var i = 0; i < chars.length; i++) {
+                console.log(chars);
+                if (inputChar == chars[i].textContent) {
+                    chars[i].classList.remove('hidden');
+                }
+            }
+            //new global variable that is original word
+            //display that char at correct index in word status
+            //identify char selected and if it is correct
+            //push it to word status array at appropriate index
+            //join word status to display
+            //will I need to do this in a new function?
     
             return true;
         }
